ca3b6ff595989cd79f36f8dc0d0e976d7ff55394
unknown <you@example.com>
2023-08-29 10:27:38 -0300

    Projeto com Navegãção

---
+++ src/Screens/Home/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#751010',
+      alignItems:"center",
+      paddingBottom: 20,
+    },
+  
+    card: {
+      width:"95%", 
+      backgroundColor:'#000000', 
+      borderRadius:'10px', 
+      marginTop:'5%',
+    }
+
+})
+
+export default styles;
