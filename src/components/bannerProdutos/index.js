976c646b8f3e59372b698582e83d56aad613d03a
unknown <you@example.com>
2023-08-29 11:47:30 -0300

    Projeto Nav

--- src/components/bannerProdutos/index.js
+++ src/components/bannerProdutos/index.js
@@ -4,11 +4,10 @@ import styles from './style';
 
 
 export default function BannerProdutos(){
-    const imagem = Math.floor(Math.random() * 4 +1);
     return(
         <>
             <Text style = {styles.textBanner}> Mais Vendidos </Text>
-            <Image style = {styles.imagemBanner} source = {require(`../../Img/${imagem}.jpg`)} />
+            <Image style = {styles.imagemBanner} source = {require(`../../Img/fundo.jpg`)} />
         </>
     );  
 }
--- src/components/cardProdutos/style.js
+++ src/components/cardProdutos/style.js
@@ -10,40 +10,39 @@ const styles = StyleSheet.create({
     },
 
     tituloProdutos:{
-        color: '#751010',
+        color: '#FFD411',
         fontSize: 20,
         paddingTop: 8,
         fontWeight:'Bold'
     },
     marcaProdutos:{
         fontSize:13,
-        color:'#FFC433',
+        color:'#FFFF',
         paddingLeft:2,
         paddingBottom:12
     },
     corProdutos:{
         fontSize:12,
-        color:'#fff',
+        color:'#FFFF',
         paddingLeft:4,
 
     },
     quantidadeProdutos:{
         fontSize:12,
-        color:'#fff',
+        color:'#FFFF',
         paddingLeft:4
     },
     tamanhoProdutos:{
         fontSize:12,
-        color:'#fff',
+        color:'#FFFF',
         paddingLeft:4
     },
     precoProdutos:{
         fontSize:18,
-        color:'#fff',
+        color:'#FFD411',
         paddingLeft:2,
         paddingTop: 12,
         fontWeight:'Bold',
-        color:'#751010'
     },
     imagemProdutos:{
         width:'90%',
